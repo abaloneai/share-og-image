5f5d2f7d72c9ab6eb7e6306aa7997dcc27e7abd6
Meathill <meathill@gmail.com>
2024-08-04 18:03:43 +0800

    change: removed header to fix safari

--- src/app/api/dailylift/route.tsx
+++ src/app/api/dailylift/route.tsx
@@ -135,10 +135,6 @@ export async function GET(req: Request) {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,OPTIONS',
         'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-        ...hasReference && {
-          'Content-Description': 'File Transfer',
-          'Content-Disposition': 'attachment; filename="dailylift-share-quote.png"',
-        },
       },
     },
   )
